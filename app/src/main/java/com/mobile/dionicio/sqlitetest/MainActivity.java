08317007a5952d159ba24eeac0e76a00af49336a
dionicio <tec.dionicioacevedo@gmail.com>
2018-03-20 23:04:51 -0400

    Implementando el clickListener en el MainActivity

--- app/src/main/java/com/mobile/dionicio/sqlitetest/MainActivity.java
+++ app/src/main/java/com/mobile/dionicio/sqlitetest/MainActivity.java
@@ -2,9 +2,10 @@ package com.mobile.dionicio.sqlitetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String DB_NAME = "productos.db";
     private static final int DB_VERSION = 1;
 
@@ -18,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void showWelcomeMessage(){
         Toast.makeText(getApplicationContext(),"Bienvenido a TestSqlite!",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Nothing for now
     }
 }
